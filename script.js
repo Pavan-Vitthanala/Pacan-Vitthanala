d6f88718caf4b9665e0c8f290963a1eb6ae68050
Pavan-Vitthanala <159865132+Pavan-Vitthanala@users.noreply.github.com>
2024-03-01 18:43:40 -0500

    Update script.js with new script added for medium page

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-/*
-let slideIndex = 0;
-
-function showSlides() {
-  let slides = document.getElementsByClassName("slide");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 3000); 
-}
-
-showSlides();
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-*/
-
-
-
 let imageCard1 = document.getElementById("slide-1");
 let imageCard2 = document.getElementById("slide-2");
 let imageCard3 = document.getElementById("slide-3");
@@ -29,7 +6,7 @@ let imageCard5 = document.getElementById("slide-5");
 let imageCard6 = document.getElementById("slide-6");
 
 
-function myEventHandler() {
+function myEventHandlerLarge() {
   document.addEventListener("scroll", (event) => {
     let positionY = window.scrollY;
   
@@ -71,13 +48,57 @@ function myEventHandler() {
   });
 }
 
+function myEventHandlerMedium() {
+  document.addEventListener("scroll", (event) => {
+    let positionY = window.scrollY;
+  
+  
+    imageCard1.style.transform = `translateY(${
+      positionY * -0.3 + 350
+    }px)`;
+  
+    imageCard2.style.transform = `translateY(${
+      positionY * 0.1 + -50
+    }px)`;
+  
+    imageCard3.style.transform = `translateY(${
+      positionY * -0.3 + 350
+    }px)`;
+  
+    imageCard4.style.transform = `translateY(${
+      positionY * 0.1 + -50
+    }px)`;
+  
+    imageCard5.style.transform = `translateY(${
+      positionY * -0.3 + 350
+    }px)`;
+  
+  
+    imageCard6.style.transform = `translateY(${
+      positionY * 0.1 + -50
+    }px)`;
+  
+    let newOpacity = positionY * 0.001;
+    if (newOpacity >= 0 && newOpacity <= 1) {
+      imageCard1.style.opacity = newOpacity;
+      imageCard2.style.opacity = newOpacity;
+      imageCard3.style.opacity = newOpacity;
+      imageCard4.style.opacity = newOpacity;
+      imageCard5.style.opacity = newOpacity;
+      imageCard6.style.opacity = newOpacity;
+    }
+  });
+}
 
 
 
 function handleScreenSize() {
-  if (window.innerWidth > 1460) { 
-    window.addEventListener('resize', myEventHandler);
-    myEventHandler(); 
+  if (window.innerWidth > 1515) { 
+    window.addEventListener('resize', myEventHandlerLarge);
+    myEventHandlerLarge(); 
+  } else if (window.innerWidth <= 1515 && window.innerWidth > 1015) {
+    window.addEventListener('resize', myEventHandlerMedium);
+    myEventHandlerMedium();
   } else {
     window.removeEventListener('resize', myEventHandler);
   }
@@ -86,5 +107,3 @@ function handleScreenSize() {
 handleScreenSize();
 
 window.addEventListener('resize', handleScreenSize);
-
-
