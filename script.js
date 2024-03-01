058ba17e0cbd1377268cc90923491806449d0f18
Pavan-Vitthanala <159865132+Pavan-Vitthanala@users.noreply.github.com>
2024-02-29 21:17:56 -0500

    Update script.js with eventaddedrs added in JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let slideIndex = 0;
 
 function showSlides() {
@@ -16,3 +17,74 @@ showSlides();
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
+*/
+
+
+
+let imageCard1 = document.getElementById("slide-1");
+let imageCard2 = document.getElementById("slide-2");
+let imageCard3 = document.getElementById("slide-3");
+let imageCard4 = document.getElementById("slide-4");
+let imageCard5 = document.getElementById("slide-5");
+let imageCard6 = document.getElementById("slide-6");
+
+
+function myEventHandler() {
+  document.addEventListener("scroll", (event) => {
+    let positionY = window.scrollY;
+  
+  
+    imageCard1.style.transform = `translateY(${
+      positionY * -0.3 + 50
+    }px)`;
+  
+    imageCard2.style.transform = `translateY(${
+      positionY * 0.1 + -50
+    }px)`;
+  
+    imageCard3.style.transform = `translateY(${
+      positionY * -0.2 + 10
+    }px)`;
+  
+    imageCard4.style.transform = `translateY(${
+      positionY * -0.3 + 50
+    }px)`;
+  
+    imageCard5.style.transform = `translateY(${
+      positionY * 0.1 + -50
+    }px)`;
+  
+  
+    imageCard6.style.transform = `translateY(${
+      positionY * -0.2 + 10
+    }px)`;
+  
+    let newOpacity = positionY * 0.001;
+    if (newOpacity >= 0 && newOpacity <= 1) {
+      imageCard1.style.opacity = newOpacity;
+      imageCard2.style.opacity = newOpacity;
+      imageCard3.style.opacity = newOpacity;
+      imageCard4.style.opacity = newOpacity;
+      imageCard5.style.opacity = newOpacity;
+      imageCard6.style.opacity = newOpacity;
+    }
+  });
+}
+
+
+
+
+function handleScreenSize() {
+  if (window.innerWidth > 1460) { 
+    window.addEventListener('resize', myEventHandler);
+    myEventHandler(); 
+  } else {
+    window.removeEventListener('resize', myEventHandler);
+  }
+}
+
+handleScreenSize();
+
+window.addEventListener('resize', handleScreenSize);
+
+
